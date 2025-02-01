8582ab6010f1a6323a207471d25a4b11ad69411c
Kiril Radkov <kirilradkov@Macbook.local>
2025-02-01 20:33:50 +0200

    Removed Floating Pragma Usage

--- src/interfaces/launchpad/ILaunchpad.sol
+++ src/interfaces/launchpad/ILaunchpad.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity 0.8.28;
 
 interface ILaunchpad {
     function initialize(address _tokenAddress, address _wethAddress, address _uniswapRouter) external;
